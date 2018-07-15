8ce15e31b0e7bbaa4e0309e586424f3ed72dc91b
Severi <severikupari1@gmail.com>
2018-07-16 00:33:13 +0300

    Jquery clicked events added

--- pisteet.js
+++ pisteet.js
@@ -1,60 +1,110 @@
-(function(){
-    const config = {
-        apiKey: "AIzaSyBu9LYCNWvbUrpUokYDQTjaUnw8Ba7v_EI",
-        authDomain: "mahtilive.firebaseapp.com",
-        databaseURL: "https://mahtilive.firebaseio.com",
-        projectId: "mahtilive",
-        storageBucket: "mahtilive.appspot.com",
-        messagingSenderId: "847711424854"
-      };
-      firebase.initializeApp(config);
+(function () {
+  const config = {
+    apiKey: "AIzaSyBu9LYCNWvbUrpUokYDQTjaUnw8Ba7v_EI",
+    authDomain: "mahtilive.firebaseapp.com",
+    databaseURL: "https://mahtilive.firebaseio.com",
+    projectId: "mahtilive",
+    storageBucket: "mahtilive.appspot.com",
+    messagingSenderId: "847711424854"
+  };
+  firebase.initializeApp(config);
 
-      //get elements
-      const preObject = document.getElementById('object');
+  //get elements
+  const preObject = document.getElementById('object');
 
-      // Create reference
-      const dbRefObject = firebase.database().ref().child('object');
+  // Create reference
+  const dbRefObject = firebase.database().ref().child('object');
 
-      //synchronize object changes
-      dbRefObject.on('value',snap => console.log(snap.val()));
+  //synchronize object changes
+  dbRefObject.on('value', snap => console.log(snap.val()));
 
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          // User is signed in.
-          var displayName = user.displayName;
-          var email = user.email;
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var isAnonymous = user.isAnonymous;
-          var uid = user.uid;
-          var providerData = user.providerData;
-          // ...
-        } else {
-          // User is signed out.
-          // ...
-          window.location.href = "index.html"
-        }
-      });
-            
-    var logout = document.getElementById("logout");
-    logout.addEventListener("click",function(e){
-        console.log("Painettu");
-        firebase.auth().signOut().then(function() {
-            // Sign-out successful.
-          }).catch(function(error) {
-            // An error happened.
-          });
-        
-    },false);
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+      window.location.href = "index.html"
+    }
+  });
 
+  // var logout = document.getElementById("logout");
+  // logout.addEventListener("click",function(e){
+  //     console.log("Painettu");
+  //     firebase.auth().signOut().then(function() {
+  //         // Sign-out successful.
+  //       }).catch(function(error) {
+  //         // An error happened.
+  //       });
 
-
-    //Logic
-
-    var database = firebase.database();
+  // },false);
 
 
-    
+
+  //Logic
+
+  var database = firebase.database();
+
+
+
+  $(document).ready(function () {
+
+    // jQuery methods go here...
+
+    $("#mahtipisteplus").click(function () {
+      
+
+    });
+
+    $("#mahtipisteminus").click(function () {
+      
+
+    });
+
+    $("#mahtieraplus").click(function () {
+      
+
+    });
+
+    $("#mahtieraminus").click(function () {
+      
+
+    });
+
+    $("#vieraspisteplus").click(function () {
+      
+
+    });
+
+    $("#vieraspisteminus").click(function () {
+      
+
+    });
+
+    $("#vieraseraplus").click(function () {
+      
+
+    });
+
+    $("#vieraseraminus").click(function () {
+      
+
+    });
+
+
+
+  });
+
+
+
 
 
 }());
